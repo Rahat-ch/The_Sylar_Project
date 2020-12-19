23c18b5836f340a21b28762f1b9369472ded66e2
S K B <47430686+shravan20@users.noreply.github.com>
2020-12-20 03:55:04 +0530

    Add more resources #5
    **What does it add?**
    **[ADDED]** New Data from my local place
    
    **Changelogs**
    - Updated `/data/usData.js`

--- data/usData.js
+++ data/usData.js
@@ -30,5 +30,14 @@ export const usData = {
       image: 'http://dhss.alaska.gov/amhb/PublishingImages/banner-amhb.jpg',
       posted: "December 18, 2020"
     }
+  ],
+  Bengaluru: [
+    {
+      name: 'Kaha Mind',
+      link: 'https://www.kahamind.com/',
+      description: 'We believe that taking care of your mental health and wellness needs should be effortless.',
+      image: "https://pbs.twimg.com/profile_images/1010105334344478720/GwO-JUuH_400x400.jpg",
+      posted: "December 20, 2020"
+    }
   ]
 }
