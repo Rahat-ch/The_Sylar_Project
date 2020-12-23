0cfc11e527a1011687b6463047b7a57a574450b2
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-12-23 13:25:18 -0500

    update linting and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,19 @@ module.exports = {
         ecmaVersion: 2020, // Use the latest ecmascript standard
         sourceType: 'module', // Allows using import/export statements
         ecmaFeatures: {
-            jsx: true // Enable JSX since we're using React
-        }
+            jsx: true, // Enable JSX since we're using React
+        },
     },
     settings: {
         react: {
-            version: 'detect' // Automatically detect the react version
-        }
+            version: 'detect', // Automatically detect the react version
+        },
     },
     env: {
         browser: true, // Enables browser globals like window and document
         amd: true, // Enables require() and define() as global variables as per the amd spec.
         node: true, // Enables Node.js global variables and Node.js scoping.
-        jest: true
+        jest: true,
     },
     parser: 'babel-eslint',
     extends: [
@@ -26,7 +26,7 @@ module.exports = {
         'plugin:jsx-a11y/recommended',
         'airbnb',
         'prettier',
-        'prettier/react'
+        'prettier/react',
         // 'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
     ],
     plugins: ['react', 'prettier'],
@@ -36,17 +36,17 @@ module.exports = {
         'react/jsx-filename-extension': [
             1,
             {
-                extensions: ['.js', '.jsx']
-            }
+                extensions: ['.js', '.jsx'],
+            },
         ],
         'jsx-a11y/anchor-is-valid': [
             'error',
             {
                 components: ['Link'],
                 specialLink: ['hrefLeft', 'hrefRight'],
-                aspects: ['invalidHref', 'preferButton']
-            }
+                aspects: ['invalidHref', 'preferButton'],
+            },
         ],
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }] // Use our .prettierrc file as source
-    }
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    },
 };
