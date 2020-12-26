b248fc35320288ac447b0c6fd5cc30449027ab39
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-12-25 20:41:35 -0500

    add serverless backend for mental health resources

--- contexts/DropdownContext.js
+++ contexts/DropdownContext.js
@@ -5,8 +5,8 @@ export const DropdownContext = createContext(null)
 
 /* eslint react/prop-types: 0 */
 export function DropdownProvider({ children }) {
-    const apiData = useContext(ApiContext)
-    const dropDownData = ['Choose a Location', ...Object.keys(apiData)]
+    const { data } = useContext(ApiContext)
+    const dropDownData = [' Choose a Location', ...Object.keys(data)].sort()
     const [location, setLocation] = useState(dropDownData[0])
     return (
         <DropdownContext.Provider value={{ dropDownData, location, setLocation }}>
---
+++ pages/api/getResources.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+import Airtable from 'airtable'
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    process.env.AIRTABLE_BASE_ID
+)
+
+const table = base(process.env.AIRTABLE_TABLE_NAME)
+
+export default async (req, res) => {
+    const records = await table.select({}).all()
+    res.statusCode = 200
+    console.log(records)
+    const resourceMap = {}
+    records.forEach((state) => {
+        console.log(state._rawJson.fields.stateId)
+        console.log(state._rawJson.fields['State (from stateId)'])
+        const stateKey = state._rawJson.fields['State (from stateId)'][0]
+        const { isApproved } = state._rawJson.fields
+        if (isApproved && resourceMap[stateKey]) {
+            resourceMap[stateKey].push(state._rawJson.fields)
+        } else if (isApproved) {
+            resourceMap[stateKey] = [state._rawJson.fields]
+        }
+    })
+    res.json(resourceMap)
+}
+
+// for local uncomment everything below and comment everything above if you do not have access to airtable
+
+// import usData from '../../data/usData'
+
+// export default function handler(req, res) {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'application/json')
+//     res.end(JSON.stringify(usData))
+// }
