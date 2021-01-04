9145d958607b3c5eff1725496448c8c3004245ed
Jordan <jordanleelong5@gmail.com>
2021-01-03 21:10:25 -0800

    moved The Sylar Project h1 into nav, changed color to white and displayed icon image & h1 as flex items-start

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -6,25 +6,21 @@ const Layout = ({ children }) => (
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
-                        <div className="flex-shrink-0">
+                        <div className="flex-shrink-0 flex items-start">
                             <img
                                 className="h-8 w-8"
                                 src="https://i.ibb.co/027B51g/icon1.png"
                                 alt="Workflow"
                             />
+                            <h1 className="text-2xl font-bold leading-tight text-white pl-3 ">
+                                The Sylar Project
+                            </h1>
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
 
-        <header className="bg-white shadow">
-            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-3xl font-bold leading-tight text-gray-900">
-                    The Sylar Project
-                </h1>
-            </div>
-        </header>
         <main>
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 {/* <!-- Replace with your content --> */}
